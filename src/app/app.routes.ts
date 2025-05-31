62b2f19de96e716d50127d21c2d4bc5c78fbea16
Krzysztof <krzysztof.koczy99@gmail.com>
2025-05-31 11:40:32 +0200

    Config Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,35 +2,12 @@ import type { Routes } from "@angular/router"
 import { Home } from "./features/home/home/home";
 import { Thresholds } from "./features/thresholds/thresholds";
 
-
 export const routes: Routes = [
     { path: '', component: Home, },
 
     { path: 'home', component: Home },
-    { path: 'thresholds', component: Thresholds },
 
-    // { path: 'about', component: About },
-    // { path: 'contact', component: Contact },
+    { path: 'thresholds', component: Thresholds },
 
     { path: '**', component: Home }
 ];
-
-// export const routes: Routes = [
-//   {
-//     path: "",
-//     redirectTo: "/home",
-//     pathMatch: "full",
-//   },
-//   {
-//     path: "home",
-//     loadComponent: () => import("./features/home/home").then((m) => m.HomeComponent),
-//   },
-// //   {
-// //     path: "thresholds",
-// //     loadComponent: () => import("./features/thresholds/thresholds").then((m) => m.Tresholds),
-// //   },
-//   {
-//     path: "**",
-//     redirectTo: "/home",
-//   },
-// ]
